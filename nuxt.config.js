941fb02eb400969c5b4d4f3db82a55ba11a8f754
lucien <44745575+lucien2018@users.noreply.github.com>
2022-01-21 11:05:43 +0000

    succes

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    //'bootstrap-vue/nuxt',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
